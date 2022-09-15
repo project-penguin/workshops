42ad5ba5318bc47516a38e98993297d19db00ca8
project-penguin <codepenguin0s@gmail.com>
2022-09-15 20:01:55 +0800

    add cors and cache

--- Workshop5-api/util/app.js
+++ Workshop5-api/util/app.js
@@ -1,10 +1,8 @@
 import express from "express"
 import cors from "cors"
-
 const app = express()
 
-app.use(express.json({ limit: "200mb" }))
+app.use(express.json({ limit: "40MB"}))
 app.use(cors())
-// app.options(cors())
 
 export default app
